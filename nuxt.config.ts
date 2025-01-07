e0424a78551ef85d8602d53225b5b8f8c32f1d84
schwince <info@schwince.de>
2025-01-07 20:20:06 +0100

    Add robots.txt and robots Nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: "@nuxt-themes/docus",
-  modules: ["@nuxtjs/plausible"],
+
+  modules: ["@nuxtjs/plausible", "@nuxtjs/robots"],
   devtools: { enabled: false },
+  compatibilityDate: "2025-01-07",
 });
